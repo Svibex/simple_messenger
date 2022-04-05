5b213370e571e95c7ea15b66760cf85fe745e002
Svibex <future.science@yandex.ru>
2022-04-06 00:42:28 +0300

    feat: update files

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+    }
+});
 
 const rooms = new Map();
 
--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import io from 'socket.io-client';
 
 function App() {
-    return <div className="App">Hello</div>
+    const connectSocket = () => {
+        io('http://localhost:8080');
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="join-block">
+                <input
+                    type="text"
+                    placeholder="Room ID"
+                />
+                <input
+                    type="text"
+                    placeholder="Введите имя"
+                />
+                <button>
+                    ТЫК
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
